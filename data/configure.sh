8049fc7f6e4adfa81f02af9c93e516fbe7ca6ad2
ManuKuma <public@bitcraft.org>
2016-02-04 13:13:06 +0100

    + ‘data/configure.sh’

---
+++ data/configure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export site_title="JoElKu"
+export theme_dir="theme1"
+export backgroundcolor="#000000" # slide background, visible only before image has loaded
+export textcolor="#ffffff" # default text color
+export text_toggle=false
+export social_button=false
+export download_button=false
+export download_readme="All rights reserved"
+echo $site_title
